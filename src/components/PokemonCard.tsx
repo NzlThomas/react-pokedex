7cffa62f45dc7ab8b6d98e2e4595f4f64f6bf547
NzlThomas <174131483+NzlThomas@users.noreply.github.com>
2024-10-23 15:21:04 +0200

    added new pokemon and conditions if image isn't available

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,11 +1,24 @@
 function PokemonCard() {
 
+    const pokemon = pokemonList[0];
+
     return (
     <figure>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"/>
-        <figcaption>Bulbasaur</figcaption>
+        {pokemon.imgSrc ? <img src={pokemon.imgSrc}/> : <p>???</p>}
+        <figcaption>{pokemon.name}</figcaption>
     </figure>
     );
 };
+
+const pokemonList = [
+    {
+        name: "Bulbasaur",
+        imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+        name: "Mew",
+    },
+];
 
 export default PokemonCard;
