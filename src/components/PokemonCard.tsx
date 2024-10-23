9eb5714de1ae9eb7adfd8e42e203b881b50f8e45
NzlThomas <174131483+NzlThomas@users.noreply.github.com>
2024-10-23 18:31:55 +0200

    added prop for PokemonCard

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,6 +1,4 @@
-function PokemonCard() {
-
-    const pokemon = pokemonList[0];
+function PokemonCard({ pokemon }) {
 
     return (
     <figure>
@@ -10,15 +8,6 @@ function PokemonCard() {
     );
 };
 
-const pokemonList = [
-    {
-        name: "Bulbasaur",
-        imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-        name: "Mew",
-    },
-];
+
 
 export default PokemonCard;
